3e29f459e3224bb82bb0e5a6a5313e87d13e6062
KingPC <king@bogon.(none)>
2016-01-06 11:28:22 +0800

    modified app/__init__.py with SQLALCHEMY_TRACK_MODIFICATIONS True

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 db = SQLAlchemy(app)
 
 from app import views, models, controllers
