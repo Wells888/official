2a9d9f1f7388cf11b4acec2ac7002b87e4d7dbb5
ben <ben@localhost.localdomain>
2017-11-08 14:32:36 +0800

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#virtenv = "/home/ben/python_env/"
+#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+#try:
+#    execfile(virtualenv, dict(__file__=virtualenv))
+#except IOError:
+#    pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
@@ -19,7 +20,7 @@ from officialapp import app as application
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+    httpd = make_server('localhost', 8080, application)
     # Wait for a single request, serve it and quit.
     httpd.handle_request()
     #httpd.serve_forever()
