79c3abc7f138dcbfa9e6fcd1d725499f803b2886
ben <ben@localhost.localdomain>
2017-11-08 09:27:56 +0800

    171108 setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='official',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='flask App',
+      author='Ben',
+      author_email='ben@hopolite.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=[
+	    'Flask==0.10.1',
+		'SQLAlchemy==2.1'
+	  ]
      )
